61f6740a834a1e3d9d273cab88e6688d3bc32821
Kaung Khant Thu <kaungkhant.kt96@gmail.com>
2016-09-04 16:45:10 +0630

    color like primary dark brown and accent green added to the colors.xml

--- PADC_FP_YAEPYAYPAR/app/src/main/java/com/padc/yaepyaypar/activities/HomeActivity.java
+++ PADC_FP_YAEPYAYPAR/app/src/main/java/com/padc/yaepyaypar/activities/HomeActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.padc.yaepyaypar.R;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
