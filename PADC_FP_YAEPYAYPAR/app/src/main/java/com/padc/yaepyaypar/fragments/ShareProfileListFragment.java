ad0ae5e57dbf294a96cf71f0f73085cf15cf9adb
minkyawthu <compaqmkt@gmail.com>
2016-09-11 16:27:45 +0630

    Adding  friendlistfragment, shareprofilelistfragment and friendslistadapter class.

---
+++ PADC_FP_YAEPYAYPAR/app/src/main/java/com/padc/yaepyaypar/fragments/ShareProfileListFragment.java
@@ -0,0 +1,61 @@
+package com.padc.yaepyaypar.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.padc.yaepyaypar.R;
+import com.padc.yaepyaypar.adapters.FriendsListAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by mkt on 9/11/2016.
+ */
+public class ShareProfileListFragment extends Fragment {
+
+    @BindView(R.id.rv_friends)
+    RecyclerView rvFriends;
+
+    private FriendsListAdapter mFriendListAdapter;
+    private int gridColumnSpanCount = 2;
+
+    public ShareProfileListFragment() {
+    }
+
+    public static Fragment newInstance() {
+        Fragment fragment = new ShareProfileListFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String[] nameListArray = getResources().getStringArray(R.array.dummy_friends_name);
+        List<String> friendNameList = new ArrayList<>(Arrays.asList(nameListArray));
+
+        mFriendListAdapter = new FriendsListAdapter(friendNameList);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, rootView);
+
+        rvFriends.setLayoutManager(new GridLayoutManager(getContext(), gridColumnSpanCount));
+        rvFriends.setAdapter(mFriendListAdapter);
+
+        return rootView;
+    }
+}
